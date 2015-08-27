0d347270e5b92bb799ab968ca1c2639da2af559b
Simon Richards <simon@simonrichards.me>
2015-08-27 13:42:56 +1200

    Fix test setup and cleanup

---
+++ tests/AssemblySetUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using ClangSharp;
+using tests.Properties;
+
+namespace tests {
+	[SetUpFixture]
+	public class AssemblySetUp : TestBase {
+		[SetUp]
+		public static void CreateTestFiles() {
+			System.IO.File.WriteAllText(FakeClassCpp, Resources.fake_class_cpp);
+			System.IO.File.WriteAllText(FakeClassH, Resources.fake_class_h);
+			System.IO.File.WriteAllText(OpaqueClassH, Resources.opaque_class_h);
+			System.IO.File.WriteAllText(MainCpp, Resources.fake_main_cpp);
+			System.IO.File.WriteAllText(KitchenSinkCpp, Resources.kitchen_sink);
+			var args = new[] { Options.Weverything };
+			var unsavedFiles = new UnsavedFile[] { };
+			var options = TranslationUnitFlags.IncludeBriefCommentsInCodeCompletion | TranslationUnitFlags.DetailedPreprocessingRecord;
+			Index = new Index();
+			Main = Index.CreateTranslationUnit(MainCpp, args, unsavedFiles, options);
+			Class = Index.CreateTranslationUnit(FakeClassCpp, args, unsavedFiles, options);
+			KitchenSink = Index.CreateTranslationUnit(KitchenSinkCpp, args, unsavedFiles, options);
+		}
+
+		[TearDown]
+		public static void DeleteTestFiles() {
+			foreach (string file in new[] { FakeClassCpp, FakeClassH, OpaqueClassH, MainCpp, KitchenSinkCpp }) {
+				System.IO.File.Delete(file);
+			}
+			Main.Dispose();
+			Index.Dispose();
+		}
+	}
+}
+
--- tests/TestBase.cs
+++ tests/TestBase.cs
@@ -4,7 +4,6 @@ using NUnit.Framework;
 using tests.Properties;
 
 namespace tests {
-    [SetUpFixture]
     public class TestBase {
         protected static string FakeClassCpp = Path.Combine(Path.GetTempPath(), "fake-class.cpp");
         protected static string FakeClassH = Path.Combine(Path.GetTempPath(), "fake-class.h");
@@ -16,30 +15,5 @@ namespace tests {
         protected static TranslationUnit Main;
         protected static TranslationUnit Class;
         protected static TranslationUnit KitchenSink;
-
-        [SetUp]
-        public static void CreateTestFiles() {
-            System.IO.File.WriteAllText(FakeClassCpp, Resources.fake_class_cpp);
-            System.IO.File.WriteAllText(FakeClassH, Resources.fake_class_h);
-            System.IO.File.WriteAllText(OpaqueClassH, Resources.opaque_class_h);
-            System.IO.File.WriteAllText(MainCpp, Resources.fake_main_cpp);
-            System.IO.File.WriteAllText(KitchenSinkCpp, Resources.kitchen_sink);
-            var args = new[] { Options.Weverything };
-            var unsavedFiles = new UnsavedFile[] { };
-            var options = TranslationUnitFlags.IncludeBriefCommentsInCodeCompletion | TranslationUnitFlags.DetailedPreprocessingRecord;
-            Index = new Index();
-            Main = Index.CreateTranslationUnit(MainCpp, args, unsavedFiles, options);
-            Class = Index.CreateTranslationUnit(FakeClassCpp, args, unsavedFiles, options);
-            KitchenSink = Index.CreateTranslationUnit(KitchenSinkCpp, args, unsavedFiles, options);
-        }
-
-        [TearDown]
-        public static void DeleteTestFiles() {
-            foreach (string file in new[] { FakeClassCpp, FakeClassH, OpaqueClassH, MainCpp, KitchenSinkCpp }) {
-                System.IO.File.Delete(file);
-            }
-            Main.Dispose();
-            Index.Dispose();
-        }
     }
 }
